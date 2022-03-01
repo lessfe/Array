7a45dc573239601a87fea874fc9033ff290d1a6c
lessfe <100286017+lessfe@users.noreply.github.com>
2022-03-01 16:59:57 +0300

    Add files via upload

---
+++ Array/Array.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <array>
+using namespace std;
+
+void functionfor_arr(int arr[5])
+{
+	cout << sizeof(arr);
+}
+
+void functionforarr(array <int, 5> &arr)
+{
+	cout << sizeof(arr);
+
+}
+
+int main()
+{
+
+	array<int, 5> arr;
+	cout << arr.empty();
+	//int _arr[5] = { 1,2,3,4,5 };
+	//array <int, 5> arr = { 1, 2,3, 4, 5 };
+	///*functionfor_arr(_arr);*/
+	//functionforarr(arr);
+
+}
+
